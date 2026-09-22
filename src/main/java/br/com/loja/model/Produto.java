eb704fa1b7dbafd4013893a00abc3a038840865d
MARCIO COELHO ELIAS JUNIOR <marcio.elias@aluno.senac.rio>
2026-09-21 21:26:20 -0300

    aula 21092026

--- src/main/java/br/com/loja/model/Produto.java
+++ src/main/java/br/com/loja/model/Produto.java
@@ -1,14 +1,37 @@
 package br.com.loja.model;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name="produtos")
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+
+    @Column(nullable = false, length = 100)
     private String nome;
+
+    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal preco;
 
+    @Column(nullable = false, length = 255)
+    private String descricao;
+
     public Produto(){}
+    public Produto(Long id, Integer quantidade, String nome, BigDecimal preco, String descricao) {
+        this.id = id;
+        this.quantidade = quantidade;
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+    }
 
     public Long getId() {
         return id;
@@ -16,6 +39,22 @@ public class Produto {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
     }
 
     public String getNome() {
@@ -26,11 +65,11 @@ public class Produto {
         this.nome = nome;
     }
 
-    public BigDecimal getPreco() {
-        return preco;
+    public String getDescricao() {
+        return descricao;
     }
 
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 }
